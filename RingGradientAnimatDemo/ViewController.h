f4ab147ecbc4dc122cf40e22d7d6b24700242cf5
程科 <609980487@qq.com>
2017-12-21 10:52:07 +0800

    Initial Commit

---
+++ RingGradientAnimatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RingGradientAnimatDemo
+//
+//  Created by 程科 on 2017/12/21.
+//  Copyright © 2017年 程科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RingGradientAnimatDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RingGradientAnimatDemo
+//
+//  Created by 程科 on 2017/12/21.
+//  Copyright © 2017年 程科. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
